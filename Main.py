7a5fe4fe5530f5375be5815f45aa755f86952f51
Jim Stewart <jim.stewart@thelangschool.org>
2025-03-13 13:17:05 -0400

    Assessment class

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from DataReader import read_names
+from random import choice
+from Student import Student
 
 names = read_names()
 
@@ -8,5 +10,21 @@ history_classes = ["Social Studies", "American History", "World History"]
 electives = ["Art", "Music", "Coding", "Wood shop"]
 ela_classes = ["ELA 1", "ELA 2", "American Literature", "World Literature"]
 language_classes = ["Spanish 1", "Spanish 2", "French 1", "French 2", "Sign language"]
+assignment_types = ["HW","Project","Quiz","Test","Final"]
+subjects = [math_classes,science_classes,history_classes,electives,language_classes]
 student_count = 500
 
+student_names = []
+for i in range(student_count):
+    n = ""
+    while not n or n in student_names:
+        n = choice(student_names)
+    student_names.append(n)
+
+students = []
+for sn in student_names:
+    classes=[]
+    for subject in subjects:
+        classes.append(choice(subject))
+    name_split = sn.split()
+    students.append(Student(name_split[0], name_split[1], classes))
