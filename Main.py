30346f06c7b0747ff0bb071a812fde684037fbe8
Jim Stewart <jim.stewart@thelangschool.org>
2025-03-17 08:42:12 -0400

    added NTI and EXC and fixed numbering

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
+from types import new_class
+
 from Assessment import Assessment
 from DataReader import read_names
-from random import choice, gauss
+from random import choice, gauss, randint
 from Student import Student
 from Clasz import Clasz
 
@@ -33,11 +35,13 @@ for sn in student_names:
     students.append(Student(name_split[0], name_split[1], classes))
 
 classes = []
+new_class = None
 for subject in subjects:
     for option in subject:
         new_class = Clasz(option)
-    for i,assignment in enumerate(assignment_types):
-        new_class.assessments.append(assignment+str(i+1))
+    for assignment in assignment_types:
+        for i in range(assignment_types[assignment]):
+            new_class.assessments.append(assignment+str(i+1))
     classes.append(new_class)
 
 for student in students:
@@ -50,6 +54,7 @@ for student in students:
 for clasz in classes:
     print(clasz.name)
     f = open("data/"+clasz.name, "w")
+    f.write("Name,")
     for i,assessment in enumerate(clasz.assessments):
         f.write(assessment)
         if i<len(clasz.assessments)-1:
@@ -57,11 +62,22 @@ for clasz in classes:
     f.write("\n")
     for student in clasz.students:
         f.write(student.first_name + " " + student.last_name + ",")
+        ability = -1
+        while ability < 0 or ability > 100:
+            ability = gauss(75, 10)
+        consistency = -1
+        while consistency < 0 or consistency > 100:
+            consistency = gauss(95, 5)
         for i,assessment in enumerate(clasz.assessments):
-            grade = -1
-            while grade < 0 or grade > 100:
-                grade = round(gauss(75, 10))
-            f.write(str(grade))
+            if randint(0,100) > consistency:
+                f.write("NTI")
+            elif randint(0,100) < 5:
+                f.write("EXC")
+            else:
+                grade = -1
+                while grade < 0 or grade > 100:
+                    grade = round(gauss(ability, 10))
+                f.write(str(grade))
             if i < len(clasz.assessments) - 1:
                 f.write(",")
         f.write("\n")
